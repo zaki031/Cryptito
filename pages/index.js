48030e6dacd443c69fdd56ca4d5c243692714ee0
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-01-12 22:26:38 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import bg from "../public/bg.jpg";
 import pic from "../public/pic.png";
 import Image from "next/image";
-import Navbar from "../comps/navbar";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
     <>
     <Navbar/>    
 
-    <div className="flex justify-between  text-white h-[100vh] landing bg-light_bg">
+    <div className="flex justify-between  text-white h-[90vh] landing bg-hero">
       <div className="mt-[150px] txt">
         <h1 className="mx-20 font-semibold text-9xl text-dark_bg">Cryptito</h1>
      <p className="mx-20 mt-10 text-3xl text-dark_bg">For live crypto currency news and prices</p>
      <div className="flex mx-20  mt-10 gap-[80px] bh-w ">
       <div className="">
-      <button className="text-xl py-5 px-[80px]  see_more rounded-xl bg-main_color">See more <i class="uil uil-arrow-down"></i></button>
+      <button className="text-xl py-5 px-[80px]  see_more rounded-xl bg-dark_bg">See more <i class="uil uil-arrow-down"></i></button>
 
       </div>
       <div className="">
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       colors:{
         'light_bg': '#F6F9FC',
         'dark_bg':'#0A2540',
-        'main_color':'#51c1e1',
+        'main_color':'#365BFF',
       },
       backgroundImage: {
         'hero': "url('../public/bg.jpg')",
