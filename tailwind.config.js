23386dcc14af6f1f5cc840c46f582b2a3dec83ce
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-01-12 20:21:28 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'light_bg': '#F6F9FC',
+        'dark_bg':'#0A2540',
+        'main_color':'#51c1e1',
+      },
+      backgroundImage: {
+        'hero': "url('../public/bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
