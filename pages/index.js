1ec82706e1ee488a0d5cac84824c2b37853d1084
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-01-26 23:00:17 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ var scroller = Scroll.scroller;
           <div className="flex mx-20  mt-10 gap-[80px] bh-w ">
             <div className="">
               <button  className="text-md py-5 px-[80px] text-dark_bg  see_more rounded-xl bg-main_color">
-                See more <i class="uil uil-arrow-down"></i>
+                See more <i className="uil uil-arrow-down"></i>
               </button>
             </div>
             <div className="mt-[20px]">
@@ -62,7 +62,7 @@ var scroller = Scroll.scroller;
                 href="https://github.com/zaki031/Cryptito"
                 className="p-4 border-4 text-md mt- rounded-xl hover:bg-dark_bg hover:text-light_bg text-dark_bg border-dark_bg"
               >
-                Github repo <i class="uil uil-arrow-up-right"></i>
+                Github repo <i className="uil uil-arrow-up-right"></i>
               </a>
             </div>
           </div>
@@ -73,7 +73,7 @@ var scroller = Scroll.scroller;
       </div>
 
       <div className="prices-container bg-hero">
-        <div class="custom-shape-divider-bottom-1673602727 ">
+        <div className="custom-shape-divider-bottom-1673602727 ">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -82,7 +82,7 @@ var scroller = Scroll.scroller;
           >
             <path
               d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              class="shape-fill fill-light_bg"
+              className="shape-fill fill-light_bg"
             ></path>
           </svg>
         </div>
--- pages/search/[id].js
+++ pages/search/[id].js
@@ -1,13 +1,48 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../components/navbar";
+import axios from "axios";
 
-
-const coin = () => {
+const coin = ({ data }) => {
+    const [coins,SetCoins] = useState([])
     return ( 
+        useEffect(()=>{
+           
+
+        },[]),
         <>
         <Navbar/>
+        <h4>{data[0].id}</h4>
+
+
+
+        
+
+
         
         </>
      );
 }
  
 export default coin;
+
+
+
+
+export async function getServerSideProps({ params }) {
+    const { id } = params;
+   
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/search?query=${id}`
+    );
+    const data = response.data.coins;
+ 
+ 
+    return {
+      props: {
+        key: id,
+        data,
+        
+      },
+    };
+  }
+  
