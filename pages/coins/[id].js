cd60fe918b6ac54cdf32967b0cfe7490122d9d74
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-01-28 09:32:26 +0100

    first commit

--- pages/coins/[id].js
+++ pages/coins/[id].js
@@ -5,7 +5,7 @@ const coin = ({ data }) => {
   return (
     <div className="h-[100vh] ">
       <Navbar />
-      <div className="bg-light_bg rounded-[10px] p-2 m-4 coin_info">
+      <div className="bg-light_bg rounded-[10px] h-full p-2 m-4 coin_info">
         <div className="flex justify-between txt">
           <div className="flex title">
             <Image
@@ -19,10 +19,22 @@ const coin = ({ data }) => {
             <h4 className="h-6 px-3 mt-2 rounded-md text-light_bg bg-dark_bg">
               {data.symbol}
             </h4>
+            
           </div>
+         
+          <div className="prices">
           <h1 className=" mr-[100px] text-[70px] font-bold">${data.market_data.current_price.usd}</h1>
+          <h4 className="text-dark_bg ">{data.market_data.current_price.btc} BTC</h4>
+          <h4 className="text-dark_bg ">{data.market_data.current_price.eth} ETH</h4>
+
+
+          </div>
 
         </div>
+        <div className="links ">
+            <a className="bg-dark_bg rounded-md text-white p-3" href="bitcoin.org">bitcoin.org</a>
+
+            </div>
         
       </div>
     </div>
