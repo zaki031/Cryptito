c5fd8eaee4099038ae403f7d227e449b6c9723ba
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-02-04 18:00:13 +0100

    trending alignement

--- components/trend.js
+++ components/trend.js
@@ -4,16 +4,15 @@ import axios from "axios";
 import { useEffect } from "react";
 import Image from "next/image";
 const trend = () => {
-  const [trend_coins, SetTrend_coins] = useState([])
+  const [trend_coins, SetTrend_coins] = useState([]);
   useEffect(() => {
     axios
       .get("https://api.coingecko.com/api/v3/search/trending")
       .then((res) => {
-
-        SetTrend_coins(res.data.coins)
-        console.log(trend_coins)
+        SetTrend_coins(res.data.coins);
+        console.log(trend_coins);
       });
-  },[]);
+  }, []);
 
   return (
     <>
@@ -29,33 +28,32 @@ const trend = () => {
           </div>
         </div>
         <div className="text-center bg-main_color my-auto  rounded-md  lg:w-[50%] sm:m-6:w-full  ">
-          <div className="trending mt-[5px]  rounded-md m-auto bg-light_bg lg:w-[400px] sm:w-[200px] p-4">
+          <div className="trending mt-[5px]  rounded-md m-auto bg-white lg:w-[400px] sm:w-[200px] p-4">
             <div className="hightlight text-black flex justify-between">
               <h1>🔥 Trending</h1>
-              
             </div>
             <div className="coins mt-[30px]">
-                {trend_coins.map((item)=>{
-                    return(
-                        <div className="coin mb-2 mt-2 flex justify-between">
-                            <Image src={item.item.small} width={30} height={30}></Image>
-                            <div className="name flex justify-evenly w-full">
-                                                                                            <h2>{item.item.id}</h2>
-<div className="symbol text-center   ">
-                                                                                            <p className="bg-dark_bg rounded-md px-2 text-white">{item.item.symbol}</p>
-
-                            </div>
-                            </div>
-                            
-
-                            <a className="hover:bg-dark_bg p-2 transition-[0.5s] hover:text-white rounded-[50%]" href={`/coins/${item.item.id}`}>
-               <i class="uil uil-angle-right-b"></i>
-              </a>
-
+              {trend_coins.map((item) => {
+                return (
+                  <div className="coin mb-2 mt-2 p-2 rounded-xl flex hover:p-4 bg-light_bg gap-[30px] justify-evenly">
+                    <Image src={item.item.small} width={30} height={30}></Image>
+                    <div className="name flex w-full text-left   ">
+                        <div className="name_coin  w-[200px] ">
+                             <h2>{item.item.id}</h2>
 
                         </div>
-                    )
-                })}
+                      <div className="symbol text-center   ">
+                        <p className=" text-sm text-white">
+                          {item.item.symbol}
+                        </p>
+                      </div>
+                    </div>
+
+                  
+                  
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
